e2cc849e3168b00aba68c8107583f522bc66ec63
MarZaSas <m.zarcosastre@gmail.com>
2026-05-21 08:45:41 +0200

    add Validations SignUp

--- Jointless/src/app/auth/sign_up/sign_up.component.ts
+++ Jointless/src/app/auth/sign_up/sign_up.component.ts
@@ -1,32 +1,100 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, inject, signal } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
     templateUrl: 'sign_up.component.html',
-    styleUrl: '../login/login.css'
+    imports: [FormsModule],
 })
 
-export class SignUpComponent{
+export class SignUpComponent {
+
     username = signal('');
     password = signal('');
     confirmPasswd = signal('');
     email = signal('');
 
+    passwordError = signal('');
+    usernameError = signal('');
+    emailError = signal('');
+    confirmPasswdError = signal('');
+
     private http = inject(HttpClient);
 
-    sendValues(){
-        const body = {
-            user: this.username(),
-            passw: this.password(),
-            cPassw: this.confirmPasswd(),
-            email: this.email()
+    sendValues() {
+        const validPassword = this.validatePassword();
+        const validEmail = this.validateEmail();
+        const validUsername = this.validateUsername();
+        const validConfPassw = this.validateConfirmPassw();
+
+        if (validPassword && validEmail && validUsername && validConfPassw) {
+            const body = {
+                user: this.username(),
+                passw: this.password(),
+                email: this.email()
+            }
+
+            console.log(body);
+
+            this.http.post('/register', body).subscribe({
+                next: (response) => { console.log(response) }
+            });
+        }
+    }
+
+    validateUsername(): boolean {
+        if (this.username() == "") {
+            this.usernameError.set('El username es obligatorio');
+            return false;
+        }
+        this.usernameError.set('');
+        return true
+    }
+
+    validateEmail(): boolean {
+        const emailValue = this.email().trim();
+
+        if (emailValue === '') {
+            this.emailError.set('El email es obligatorio');
+            return false;
         }
 
-        console.log(body);
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) {
+            this.emailError.set('Introduce un email válido');
+            return false;
+        }
 
-        this.http.post('/register', body).subscribe({
-            next: (response) => {console.log(response)}
-        });
+        this.emailError.set('');
+        return true;
     }
+    
+    validatePassword(): boolean {
+        const passwordValue = this.password();
+        if (passwordValue.length < 4) {
+            this.passwordError.set('La contraseña debe tener mínimo 4 caracteres');
+            return false;
+        }
+
+        if (!/\d/.test(passwordValue)) {
+            this.passwordError.set('La contraseña debe incluir 1 número');
+            return false;
+        }
+        if(passwordValue!=this.confirmPasswd()) return false
+        this.passwordError.set('');
+        return true;
+    }
+
+    validateConfirmPassw(){
+        const confPasswordValue = this.confirmPasswd();
+
+        if(this.password()!=""&&this.password()!=confPasswordValue){
+            this.confirmPasswdError.set('Las contraseñas no coinciden');
+            return false;
+        }
+        this.confirmPasswdError.set('');
+        return true;
+    }
+    
+    
 }
