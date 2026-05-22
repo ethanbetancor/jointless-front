48f9b1c88985577c17e477d44d55f0571226f868
MarZaSas <m.zarcosastre@gmail.com>
2026-05-22 11:01:29 +0200

    Revert "Refactor change password"

--- Jointless/src/main.ts
+++ Jointless/src/main.ts
@@ -2,16 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-// const { worker } = await import('./mocks/browser');
-
-// await worker.start({
-//   serviceWorker: {
-//     url: '/mockServiceWorker.js'
-//   }
-// });
-
-
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
-
-
