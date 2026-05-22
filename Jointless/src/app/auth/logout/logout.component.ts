899764d6765c408299865f8723f91bf544acc183
MarZaSas <m.zarcosastre@gmail.com>
2026-05-22 11:33:33 +0200

    add logout

--- Jointless/src/app/auth/logout/logout.component.ts
+++ Jointless/src/app/auth/logout/logout.component.ts
@@ -1,7 +1,17 @@
+import { Component, inject, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
+@Component({
+  template: ''
+})
+export class LogoutComponent implements OnInit {
 
+  private router = inject(Router);
 
-export class LogoutComponent{
+  ngOnInit() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('credentials');
 
-    
+    this.router.navigateByUrl('/login');
+  }
 }
