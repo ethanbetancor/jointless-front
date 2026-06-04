753ca2b85f5d2608f58a0d4b07dac22e4bbff14f
carlosmartinmartinezmetrica <carlos.martin.martinez.metrica@gmail.com>
2026-06-04 14:07:26 +0200

    show category in history

--- Jointless/src/app/home/history/history.ts
+++ Jointless/src/app/home/history/history.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, inject, Component, OnInit, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, inject, Component, OnInit, effect, signal } from '@angular/core';
 import { Title , Meta} from '@angular/platform-browser';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { AuthService } from '../../auth/auth.service';
@@ -27,52 +27,72 @@ export class History implements OnInit{
   private url = 'http://localhost:8080';
   titleExercise = signal('');
   private authService = inject(AuthService);
-  exercises = signal<ListSolutions>({
-    listSolutions: []
-  });
-  completed = computed<ListSolutions>(()=>{
-    const list = this.exercises();
-    if (list.listSolutions.length===0)return {
-      listSolutions: []
-    };
-    return list;
-  });
-  getSignal(): string{
-    let result=this.titleExercise();
-    return result;
-  }
-  getTitle(id:number){
-    this.authService.getLevel(id).subscribe({
-      next: (response) =>{
-        this.titleExercise.set(response.level.title);
-      }, error: (error)=>{
-        if (error.status === 404)alert('Ejercicio no accesible');
-        alert('Error del servidor');
+  exercises = signal<ListSolutions>({ listSolutions: [] });
+  titles = signal<Record<number, string>>({});
+  statement = signal<Record<number, string>>({});
+  titlesEffect = effect(() => {
+    const list = this.exercises().listSolutions;
+    if (!list.length) return;
+    const uniqueIds = [...new Set(list.map(x => x.levelId))];
+    uniqueIds.forEach(id => {
+      if (!this.titles()[id]) {
+        this.loadTitle(id);
       }
-    })
+    });
+  });
+  loadTitle(id: number) {
+    this.authService.getLevel(id).subscribe({
+      next: (response) => {
+        this.titles.update(current => ({
+          ...current,
+          [id]: response.level.title
+        }));
+      }
+    });
+  }
+  statementEffect = effect(() => {
+    const list = this.exercises().listSolutions;
+    if (!list.length) return;
+    const uniqueIds = [...new Set(list.map(x => x.levelId))];
+    uniqueIds.forEach(id => {
+      if (!this.statement()[id]) {
+        this.loadStatement(id);
+      }
+    });
+  });
+  loadStatement(id: number) {
+    this.authService.getLevel(id).subscribe({
+      next: (response) => {
+        this.statement.update(current => ({
+          ...current,
+          [id]: response.level.category
+        }));
+      }
+    });
   }
   private title=inject(Title);
   private meta=inject(Meta);
   ngOnInit(): void {
-      const token = localStorage.getItem('token');
-  
-      if (!token) {
-        throw new Error('No token stored');
+    const token = localStorage.getItem('token');
+    if (!token) throw new Error('No token stored');
+    const headers = new HttpHeaders({
+      authorization: `Bearer ${token}`
+    });
+    this.http.post<ListSolutions>(
+      `${this.url}/api/v1/solutions/user`,
+      {},
+      { headers }
+    ).subscribe({
+      next: (response) => {
+        this.exercises.set(response);
+      },
+      error: () => {
+        alert('Error del servidor');
       }
-      const headers = new HttpHeaders({
-        authorization: `Bearer ${token}`
-      });
-      this.http.post<ListSolutions>(`${this.url}/api/v1/solutions/user`,{},{headers}).subscribe({
-        next: (response: ListSolutions)=>{
-          this.exercises.set(response);   
-        }, error: (error)=>{
-          if (error.status === 404)alert('Ejercicio no accesible');
-          alert('Error del servidor');
-        }
-      })
-      this.title.setTitle('Home');
-      this.meta.updateTag({name:'description',content:'Este es mi Home'});
-      this.meta.updateTag({name:'og:title',content:'Home'});
-      this.meta.updateTag({name:'keywords',content:'Jointless,Proyecto,Metrica,Home'});
-    }
+    });
+    this.title.setTitle('Home');
+    this.meta.updateTag({ name: 'description', content: 'Este es mi Home' });
+    this.meta.updateTag({ name: 'og:title', content: 'Home' });
+    this.meta.updateTag({ name: 'keywords', content: 'Jointless,Proyecto,Metrica,Home' });
+  }
 }
